5b536c57081e3e6871faad772125d7c36c73bb4e
shashwat patel <shashwatpatel04@gmail.com>
2025-01-31 13:22:27 +0530

    console app created , CLI tools

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the My Console Application!");
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter your country: ");
+        string country = Console.ReadLine();
+
+        Console.WriteLine("\n User Information .....");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Country: {country}");
+
+    }
+}
